973aa6f99d545bbea20805d4a22cb3f79c7bf926
SteveBreen2 <teediam@hotmail.com>
2020-08-13 01:26:31 +0100

    nightly build

--- TetrisButLessFun-HTML5-Shipping.data.js
+++ TetrisButLessFun-HTML5-Shipping.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/TetrisButLessFun/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 105, "filename": "/UE4CommandLine.txt"}, {"start": 105, "audio": 0, "end": 17372376, "filename": "/TetrisButLessFun/Content/Paks/TetrisButLessFun-HTML5.pak"}], "remote_package_size": 17372376, "package_uuid": "304f290e-1c0f-40d7-8b0d-2703690c0859"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 105, "filename": "/UE4CommandLine.txt"}, {"start": 105, "audio": 0, "end": 18196578, "filename": "/TetrisButLessFun/Content/Paks/TetrisButLessFun-HTML5.pak"}], "remote_package_size": 18196578, "package_uuid": "8e1a2cab-e0d5-46ba-b879-8978b323f319"});
 
 })();
